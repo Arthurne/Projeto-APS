6515efaa7175a3c8687f47832a9ada309be94e35
Arthur Neto <100531781+Arthurne@users.noreply.github.com>
2026-08-05 15:21:53 -0300

    Delete src directory

--- src/UnidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padaria.repository;
-
-import com.padaria.model.Movimentacao;
-import java.util.List;
-
-public interface MovimentacaoRepository {
-    void salvar(Movimentacao movimentacao);
-    List<Movimentacao> listarTodos();
-}
